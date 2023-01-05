8676e7b610b69f44c4fb61aa812d6f960edf350f
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-01-04 21:23:22 -0800

    Resize examples

--- resize_examples.py
+++ resize_examples.py
@@ -6,7 +6,7 @@ def resize_examples():
 
     for n in os.listdir(path):
         img = cv2.imread(os.path.join(path, n))
-        img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_NEAREST)
+        img = cv2.resize(img, (192, 192), interpolation=cv2.INTER_NEAREST)
         cv2.imwrite(os.path.join(path, n), img)
 
 if __name__ == "__main__":
